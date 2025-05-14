b5a5ffdc2241ae4dae05ce5850fcfc296a2b569f
Ivan Zakharchanka <3axap4eHko@gmail.com>
2025-05-14 17:08:03 -0400

    fixes build test

--- src/__tests__/index.cjs
+++ src/__tests__/index.cjs
@@ -1,8 +1,8 @@
 const { strict: assert } = require('node:assert');
-const { createContext, Context, contextualize } = require('../../build/index.cjs');
+const { createContext, Contextable, contextualize } = require('../../build/index.cjs');
 
 assert(typeof createContext === 'function');
-assert(typeof Context === 'function');
+assert(typeof Contextable === 'function');
 assert(typeof contextualize === 'function');
 
 console.log('CJS import test passed');
--- src/__tests__/index.mjs
+++ src/__tests__/index.mjs
@@ -1,8 +1,8 @@
 import { strict as assert } from 'node:assert';
-import { createContext, Context, contextualize } from '../../build/index.js';
+import { createContext, Contextable, contextualize } from '../../build/index.js';
 
 assert(typeof createContext === 'function');
-assert(typeof Context === 'function');
+assert(typeof Contextable === 'function');
 assert(typeof contextualize === 'function');
 
 console.log('MJS import test passed');
